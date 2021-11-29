acf55a1eb4cfadd7d976cee67822feb87ae7fad8
Andrey Kurakov <kurakov@eticon.ru>
2021-11-29 12:42:30 +0700

    Tests

--- lib/eticon_extension.dart
+++ lib/eticon_extension.dart
@@ -1,6 +1,6 @@
 library eticon_extension;
 
-extension ExtensionString on String?{
+extension ExtensionNullString on String?{
 
   bool get isNullOrEmpty => _isNullOrEmpty();
 
@@ -12,5 +12,53 @@ extension ExtensionString on String?{
       return true;
     }
     return false;
+  }
+}
+
+extension ExtensionString on String{
+  int toInt(){
+    return int.parse(this);
+  }
+  double toDouble(){
+    return double.parse(this);
+  }
+}
+
+extension ExtensionDateTime on DateTime{
+  bool operator >(DateTime other){
+    if(isAfter(other)){
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+  bool operator >=(DateTime other){
+    if(isAfter(other) || this == other){
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+  bool operator <=(DateTime other){
+    if(isBefore(other) || this == other){
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+  bool operator <(DateTime other){
+    if(isBefore(other) || this == other){
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  DateTime operator -(DateTime other){
+    return DateTime.fromMillisecondsSinceEpoch(this.millisecondsSinceEpoch - other.millisecondsSinceEpoch);
   }
 }
